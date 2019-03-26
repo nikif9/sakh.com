96e4ef56d8c6138c6e96865d803a0eba45114bdc
nikif9 <skylex98@mail.ru>
2019-03-27 00:57:52 +0900

    asd

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$host = 'localhost'; // адрес сервера 
-$database = 'test'; // имя базы данных
-$user = 'root'; // имя пользователя
-$password = 'duzavi26'; // пароль
+	$host = 'localhost'; // адрес сервера 
+	$database = 'test'; // имя базы данных
+	$user = 'root'; // имя пользователя
+	$password = '123'; // пароль
 ?>
